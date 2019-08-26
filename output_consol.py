bac6d040b160732ba3940447041f323aeb06990d
taklavisha <47623974+taklavisha@users.noreply.github.com>
2019-08-26 12:11:03 +0530

    Add files via upload

---
+++ output_consol.py
@@ -0,0 +1,137 @@
+Python 3.7.3 (default, Apr  3 2019, 05:39:12) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /home/cs2016a114/Desktop/lavisha python directory/displaying_name.py 
+my name is lavisha tak
+>>> 
+=== RESTART: /home/cs2016a114/Desktop/lavisha python directory/addition.py ===
+enter the value of a:20
+enter the value of b:30
+2030
+>>> 
+=== RESTART: /home/cs2016a114/Desktop/lavisha python directory/addition.py ===
+enter the value of a:20
+enter the value of b:30
+50
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> a
+[1, 2, 3, 20, 'a']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> a
+[1, 2, 3, 20, 'a', '5']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> a
+[1, 2, 3, 20, 'a', '5', 'a', 'b', 'c']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> a
+['1', 4, 5]
+>>> b
+[5, 6, 7]
+>>> a+b
+['1', 4, 5, 5, 6, 7]
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+Traceback (most recent call last):
+  File "/home/cs2016a114/Desktop/lavisha python directory/list_demo.py", line 8, in <module>
+    a.index(-1)
+ValueError: -1 is not in list
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> a
+['1', 4, 5]
+>>> b
+[5, 6, 7]
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> d
+2
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> e
+['cmr', 'is', 'good', 'college']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+Traceback (most recent call last):
+  File "/home/cs2016a114/Desktop/lavisha python directory/list_demo.py", line 11, in <module>
+    e.replace("college","university")
+AttributeError: 'list' object has no attribute 'replace'
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> e
+['cmr', 'is', 'good', 'college', 'university']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+Traceback (most recent call last):
+  File "/home/cs2016a114/Desktop/lavisha python directory/list_demo.py", line 11, in <module>
+    e.change("college","university")
+AttributeError: 'list' object has no attribute 'change'
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+Traceback (most recent call last):
+  File "/home/cs2016a114/Desktop/lavisha python directory/list_demo.py", line 11, in <module>
+    e.override("college","university")
+AttributeError: 'list' object has no attribute 'override'
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+Traceback (most recent call last):
+  File "/home/cs2016a114/Desktop/lavisha python directory/list_demo.py", line 11, in <module>
+    e.pop("college")
+TypeError: 'str' object cannot be interpreted as an integer
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+Traceback (most recent call last):
+  File "/home/cs2016a114/Desktop/lavisha python directory/list_demo.py", line 11, in <module>
+    e.pop("college")
+TypeError: 'str' object cannot be interpreted as an integer
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> e
+['cmr', 'is', 'good']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> e
+['cmr', 'is', 'good', 'university']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo.py ==
+>>> e
+['university', 'good', 'is', 'cmr']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+['lavisha', 'cmru', 8.92, '7th sem']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+['lavisha', 'cmru', 8.92, '7th sem']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+['lavisha', 'cmru', 8.92, '7th sem']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+['lavisha', 'cmru', 8.92, '7th sem']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+['lavisha', 'cmru', 8.92, '7th sem', 16]
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+['lavisha', 'cmru', 8.92, '7th sem', 16]
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+>>> b
+2
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+>>> a
+['lavisha', 'cmru', 8.92, '7th sem', 16, 'computer science']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+>>> a
+['lavisha', 8.92, 'cmr university', '7th sem', 16, 'computer science']
+>>> 
+== RESTART: /home/cs2016a114/Desktop/lavisha python directory/list_demo2.py ==
+>>> a
+['computer science', 16, '7th sem', 'cmr university', 8.92, 'lavisha']
+>>> 
